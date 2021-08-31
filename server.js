9231b8cd5384dc0beab0eab0a76e7b3bcfa070a1
Daniil Ratakhin <ratahind@mail.ru>
2021-09-01 00:48:26 +0700

    Server is done

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const nodemailer = require("nodemailer")
+
+const app = express();
+
+app.post(["/startPage", "/adminPage"], (request, response)=>{
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "ratahindipst@gmail.com",
+            pass: "IPSTOneLove"
+        }
+    })
+
+    const mailOptions = {
+        from: "ratahindipst@gmail.com",
+        to: "englishpatient.contact@gmail.com",
+        subject: "Message from the blog!",
+        text: `Имя: ${request.body.name} \nОрганизация: ${request.body.organisation} \nДолжность: ${request.body.position}
+        \nE-mail: ${request.body.email} \nТелефон: ${request.body.number}`
+    }
+
+    transporter.sendMail(mailOptions)
+})
+
+app.listen(3000);
+
+
+/*app.use(express.static("public"))
+app.use(express.json())
+app.get("/", function(request, response){
+    response.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/", (request, response)=>{
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "ratahindipst@gmail.com",
+            pass: "IPSTOneLove"
+        }
+    })
+
+    const mailOptions = {
+        from: "ratahindipst@gmail.com",
+        to: "englishpatient.contact@gmail.com",
+        subject: "Message from thr blog!",
+        text: `Hello ${request.body.number} \nwevwbenviwenv`
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if (error){
+            console.log("error")
+            response.send("error")
+        }
+        else {
+            console.log("Email was successfully sent")
+        }
+    })
+})
+
+app.listen(3000);*/
