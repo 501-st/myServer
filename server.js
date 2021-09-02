2c3123f2e54006476856c791bf1d6d41f013a76e
Daniil Ratakhin <ratahind@mail.ru>
2021-09-03 01:56:08 +0700

    aa

--- server.js
+++ server.js
@@ -2,14 +2,9 @@ import express, {json} from "express"
 import nodemailer from "nodemailer"
 
 const app = express();
-
-
 app.use(express.json())
-app.get("/", (request, response)=>{
-    console.log(request.query)
-    response.send("GG WP")
-})
-app.post("https://my-server-ipst.herokuapp.com/api/sendForm" , (request, response)=>{
+
+app.post("/sendEmail" , (request, response)=>{
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
@@ -25,7 +20,7 @@ app.post("https://my-server-ipst.herokuapp.com/api/sendForm" , (request, respons
         text: `Имя: ${request.body.name} \nОрганизация: ${request.body.organisation} \nДолжность: ${request.body.position}
 E-mail: ${request.body.email} \nТелефон: ${request.body.number}`
     }
-    response.status(200).json("Сервер работает")
+    response.status(200).json("Письмо успешно отправлено")
     console.log("Request:", request.body);
     transporter.sendMail(mailOptions)
 })
