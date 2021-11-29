bb6233cf3a14aae36c3c312cbf0467d6a5ab34a6
Daniil <64906280+501-st@users.noreply.github.com>
2021-11-29 14:37:55 +0700

    Update mailAPI.js

--- api/mailAPI.js
+++ api/mailAPI.js
@@ -2,6 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 587,
     auth: {
         user: "ratahindipst@gmail.com",
         pass: "IPSTOneLove"
